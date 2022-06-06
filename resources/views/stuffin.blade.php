dd0798a97f96eae7a3a71faa266ca2e0cb3c64a2
GusArjuna <arjunaprog@gmail.com>
2022-06-06 09:27:14 +0700

    Adding Project

---
+++ resources/views/stuffin.blade.php
@@ -0,0 +1,5 @@
+@extends('layout.navmenu')
+@section('container')
+    <!-- Page Heading -->
+    <h1 class="h3 mb-4 text-gray-800">Barang Masuk</h1>
+@endsection
---
+++ resources/views/stuffout.blade.php
@@ -0,0 +1,5 @@
+@extends('layout.navmenu')
+@section('container')
+    <!-- Page Heading -->
+    <h1 class="h3 mb-4 text-gray-800">Barang Keluar</h1>
+@endsection
