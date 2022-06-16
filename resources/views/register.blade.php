860bf5afc6692fe2d1fb107bdbb9305aedb22021
GusArjuna <arjunaprog@gmail.com>
2022-06-16 15:21:21 +0700

    adding and modified login , registration, and forgot password page (2)

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>PT. INDOTANK - Register</title>
+    <title>PT. INDOTANK - Registration</title>
 
     <!-- Custom fonts for this template-->
     <link href="{{ url('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
